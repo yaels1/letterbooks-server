9640fa142792b318dc3fd8286a1339e59627b47e
Yael Sheinman <yael.sheinman@gmail.com>
2024-01-27 16:08:27 +0000

    add function to add list of themes to each book for book endpoint

--- controllers/controller-book.js
+++ controllers/controller-book.js
@@ -3,8 +3,44 @@ require("dotenv").config();
 
 const bookData = async (_req, res) => {
   try {
-    const getBook = await knex("book");
-    res.json(getBook);
+    const getBook = await knex("book")
+      .join("author", "author.id", "book.author_id")
+      .select(
+        "book.id",
+        "book.title",
+        "book.pages",
+        "book.summary",
+        "book.image",
+        "author.name"
+      );
+
+    getBook.forEach((book) => {
+      if (book.image) {
+        book.image = `${process.env.API_URL}:${process.env.PORT}/${book.image}`;
+      }
+    });
+
+    let bookThemes = [];
+
+    for (const book of getBook) {
+      const themes = await knex("theme")
+        .join("book_theme", "theme.id", "book_theme.theme_id")
+        .where("book_theme.book_id", book.id)
+        .select("theme.name");
+
+      bookThemes.push({
+        book_id: book.id,
+        themes: themes.map((theme) => theme.name),
+      });
+    }
+    const bookAll = getBook.map((book) => {
+      const matchingThemes = bookThemes.find(
+        (item) => item.book_id === book.id
+      );
+      return { ...book, themes: matchingThemes ? matchingThemes.themes : [] };
+    });
+
+    res.status(200).json(bookAll);
   } catch (error) {
     res.status(400).send(`Error retrieving book: ${error}`);
     console.error(error);
@@ -13,34 +49,38 @@ const bookData = async (_req, res) => {
 
 const singleBook = async (req, res) => {
   // book data
-  const singleBookData = await knex("book")
-    .join("author", "author.id", "book.author_id")
-    .where({ "book.id": req.params.id })
-    .first()
-    .select(
-      "book.title",
-      "book.pages",
-      "book.summary",
-      "book.image",
-      "author.name"
+  try {
+    const singleBookData = await knex("book")
+      .join("author", "author.id", "book.author_id")
+      .where({ "book.id": req.params.id })
+      .first()
+      .select(
+        "book.title",
+        "book.pages",
+        "book.summary",
+        "book.image",
+        "author.name"
+      );
+    // image url
+    singleBookData.image = `${process.env.API_URL}:${process.env.PORT}/${singleBookData.image}`;
+
+    // book theme find
+    const singleBookThemes = await knex("theme").whereIn(
+      "id",
+      knex("book_theme").select("theme_id").where("book_id", req.params.id)
     );
-  // image url
-  singleBookData.image = `${process.env.API_URL}:${process.env.PORT}/${singleBookData.image}`;
+    // book theme list
+    const singleBookThemeNames = singleBookThemes.map((theme) => theme.name);
 
-  // book theme find
-  const singleBookThemes = await knex("theme").whereIn(
-    "id",
-    knex("book_theme").select("theme_id").where("book_id", req.params.id)
-  );
-  // book theme list
-  const singleBookThemeNames = singleBookThemes.map((theme) => theme.name);
+    // add themes to book object
+    const singleBookAll = { ...singleBookData, themes: singleBookThemeNames };
 
-  // add themes to book object
-  const singleBookAll = { ...singleBookData, themes: singleBookThemeNames };
-
-  if (!singleBookData)
-    return res.status(404).send(`Book ID:${req.params.id} not found`);
-  res.status(200).json(singleBookAll);
+    if (!singleBookData)
+      return res.status(404).send(`Book ID:${req.params.id} not found`);
+    res.status(200).json(singleBookAll);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = { bookData, singleBook };
--- seed-data/books-data.js
+++ seed-data/books-data.js
@@ -31,7 +31,7 @@ module.exports = [
     title: "A Court of Thorns and Roses",
     pages: 448,
     summary:
-      "When nineteen-year-old huntress Feyre kills a wolf in the woods, a terrifying creature arrives to demand retribution. Dragged to a treacherous magical land she knows about only from legends, Feyre discovers that her captor is not truly a beast, but one of the lethal, immortal faeries who once ruled her world",
+      "When nineteen-year-old huntress Feyre kills a wolf in the woods, a terrifying creature arrives to demand retribution. Dragged to a treacherous magical land she knows about only from legends, Feyre discovers that her captor is not truly a beast, but one of the lethal, immortal faeries who once ruled her world. At least, he’s not a beast all the time.As she adapts to her new home, her feelings for the faerie, Tamlin, transform from icy hostility into a fiery passion that burns through every lie she’s been told about the beautiful, dangerous world of the Fae. But something is not right in the faerie lands. An ancient, wicked shadow is growing, and Feyre must find a way to stop it, or doom Tamlin—and his world—forever.",
     author_id: 4,
     image: "book-covers/acotar.jpg",
   },
